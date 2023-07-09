604be01aca848ea526d59adf8fec870e1c1a318b
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-09 16:31:53 +0100

    docs: documentations added to the function

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 def canUnlockAll(boxes):
+    """
+    Determines whether all boxes can be unlocked or not.
+
+    Args:
+        boxes (list): A list of lists representing the boxes and their corresponding keys.
+            Each inner list represents a box, and the indices of the outer list represent
+            the box numbers. Each inner list contains the keys that can unlock other boxes.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
+
     num_boxes = len(boxes)
     visited = [False] * num_boxes
     visited[0] = True  # The first box is unlocked
