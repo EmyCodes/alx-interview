1813528457b74a4619281b1907a54ec97dadbc6d
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-09 16:59:23 +0100

    fix: tries to fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
+""" Defines lockboxes """
+
+
 def canUnlockAll(boxes):
-    """Determines whether all boxes can be unlocked or not.
-
-    Args:
-        boxes (list): A list of lists
-        representing the boxes and their corresponding keys.
-
-    Returns:
-        bool: True if all boxes can be unlocked, False otherwise.
-    """
-
+    """ Determines whether all boxes can be unlocked or not
+    Returns: True if all boxes can be unlocked  and
+             False if not
+             """
     keys = [0]
     for key in keys:
         for val in boxes[key]:
@@ -18,16 +15,3 @@ def canUnlockAll(boxes):
     if len(keys) == len(boxes):
         return True
     return False
-
-    # num_boxes = len(boxes)
-    # visited = [False] * num_boxes
-    # visited[0] = True  # The first box is unlocked
-    # keys = boxes[0]  # Start with the keys in the first box
-
-    # while keys:
-    #     key = keys.pop(0)
-    #     if key < num_boxes and not visited[key]:
-    #         visited[key] = True
-    #         keys.extend(boxes[key])
-
-    # return all(visited)
