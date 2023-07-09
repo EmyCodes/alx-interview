fed1b6fa4d897b192f4327569022a37c0a61c2fb
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-09 16:40:50 +0100

    feat: still working out some things

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python3
 def canUnlockAll(boxes):
-    """
-    Determines whether all boxes can be unlocked or not.
+    """Determines whether all boxes can be unlocked or not.
 
     Args:
-        boxes (list): A list of lists representing the boxes and their corresponding keys.
-            Each inner list represents a box, and the indices of the outer list represent
-            the box numbers. Each inner list contains the keys that can unlock other boxes.
+        boxes (list): A list of lists
+        representing the boxes and their corresponding keys.
 
     Returns:
         bool: True if all boxes can be unlocked, False otherwise.
     """
 
-    num_boxes = len(boxes)
-    visited = [False] * num_boxes
-    visited[0] = True  # The first box is unlocked
-    keys = boxes[0]  # Start with the keys in the first box
+    keys = [0]
+    for key in keys:
+        for val in boxes[key]:
+            if val not in keys:
+                keys.append(val)
+    if len(keys) == len(boxes):
+        return True
+    return False
 
-    while keys:
-        key = keys.pop(0)
-        if key < num_boxes and not visited[key]:
-            visited[key] = True
-            keys.extend(boxes[key])
+    # num_boxes = len(boxes)
+    # visited = [False] * num_boxes
+    # visited[0] = True  # The first box is unlocked
+    # keys = boxes[0]  # Start with the keys in the first box
 
-    return all(visited)
+    # while keys:
+    #     key = keys.pop(0)
+    #     if key < num_boxes and not visited[key]:
+    #         visited[key] = True
+    #         keys.extend(boxes[key])
+
+    # return all(visited)
