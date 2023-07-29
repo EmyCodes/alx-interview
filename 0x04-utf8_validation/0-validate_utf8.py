a403a497bbf7d0c98e7576c41b7ae24a271517c5
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-29 02:24:11 +0100

    feat:  determines if a given data set represents a valid UTF-8 encoding.

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" A method that determines if a given
+data set represents a valid UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """
+    method that determines if a given
+    data set represents a valid UTF-8 encoding.
+    """
+    for element in data:
+        UTF8_encoding = '08b'
+        char = format(element, UTF8_encoding)
+
+        if char.startswith('0'):
+            continue
+        elif char.startswith('110'):
+            continue
+        elif char.startswith("1110"):
+            continue
+        elif char.startswith("11110"):
+            continue
+        else:
+            return False
+        return True
